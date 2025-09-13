84f981e7c55001aa1d510096312eb6ef955ad58b
Arash <sainiarash555@gmail.com>
2025-09-13 18:51:04 +0530

    const

--- program2.cpp
+++ program2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     string name;
-    cout<<"enter your number___";
+    cout<<"enter your number";
     cin>>name;
     cout<<"Hello"<<name;
     return 0;
---
+++ program3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    const int a=10;
+    int b=56;
+    cout<<a<<b;
+    b=54;
+   cout<<a<<b;
+    return 0;
+
+}
